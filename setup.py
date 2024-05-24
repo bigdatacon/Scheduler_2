38974817048282066529ccd6d1883c6cb728374c
bigdatacon <link17892020@gmail.com>
2024-05-24 18:03:17 +0300

    новый актион2

--- setup.py
+++ setup.py
@@ -1,6 +1,23 @@
 from setuptools import setup, Extension
 import pybind11
 
+ext_modules = [
+    Extension(
+        "my_module",
+        sources=[
+            "my_module/module.cpp",
+            "my_module/InputData.cpp",
+        ],
+        include_dirs=[
+            pybind11.get_include(),
+            "extern/pybind11/include",
+            "json/include"
+        ],
+        language="c++",
+        extra_compile_args=["-std=c++11"]
+    ),
+]
+
 setup(
     name="my_module",
     version="1.0",
@@ -8,21 +25,7 @@ setup(
     author_email="your.email@example.com",
     description="A sample C++ extension module",
     long_description="",
-    ext_modules=[
-        Extension(
-            "my_module",
-            sources=[
-                "my_module/module.cpp",
-                "my_module/InputData.cpp",
-            ],
-            include_dirs=[
-                pybind11.get_include(),
-                "extern/pybind11/include",
-                "json/include"
-            ],
-            language="c++"
-        ),
-    ],
+    ext_modules=ext_modules,
     install_requires=[
         "pybind11>=2.5.0"
     ],
