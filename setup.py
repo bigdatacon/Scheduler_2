7c24515315fd907cbd777fea5a3b74fdc93b8305
bigdatacon <link17892020@gmail.com>
2024-05-24 14:28:37 +0300

    новая ветка - универсальный wheel

--- setup.py
+++ setup.py
@@ -1,26 +1,19 @@
 from setuptools import setup, Extension
-from setuptools.command.build_ext import build_ext
-
-class BuildExt(build_ext):
-    def build_extensions(self):
-        compiler = self.compiler.compiler_type
-        if compiler == 'unix':
-            for ext in self.extensions:
-                ext.extra_compile_args = ['-std=c++17']
-        build_ext.build_extensions(self)
-
-ext_modules = [
-    Extension(
-        'my_module',
-        sources=['my_module/module.cpp', 'my_module/InputData.cpp'],
-        include_dirs=['extern/pybind11/include', 'json/include'],
-        language='c++'
-    ),
-]
+import pybind11
 
 setup(
     name='my_module',
     version='1.0',
-    ext_modules=ext_modules,
-    cmdclass={'build_ext': BuildExt},
+    author='Your Name',
+    author_email='your_email@example.com',
+    description='A description of your project',
+    ext_modules=[
+        Extension(
+            'my_module',
+            sources=['my_module/module.cpp', 'my_module/InputData.cpp'],
+            include_dirs=[pybind11.get_include(), 'extern/pybind11/include', 'json/include'],
+            language='c++'
+        )
+    ],
+    zip_safe=False,
 )
