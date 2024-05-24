fd13b6eecf42e083b6b76f6d343cf60c81891d4e
bigdatacon <link17892020@gmail.com>
2024-05-24 13:59:54 +0300

    новая ветка - универсальный wheel

---
+++ build_wheels.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cibuildwheel --output-dir wheelhouse
--- setup.py
+++ setup.py
@@ -1,18 +1,26 @@
 from setuptools import setup, Extension
-import os
+from setuptools.command.build_ext import build_ext
 
-# Define the extension module
-module = Extension(
-    'my_module',
-    sources=['my_module/module.cpp', 'my_module/InputData.cpp'],
-    include_dirs=['./extern/pybind11/include', './json/include'],
-    extra_compile_args=['-std=c++17'],
-)
+class BuildExt(build_ext):
+    def build_extensions(self):
+        compiler = self.compiler.compiler_type
+        if compiler == 'unix':
+            for ext in self.extensions:
+                ext.extra_compile_args = ['-std=c++17']
+        build_ext.build_extensions(self)
 
-# Run the setup
+ext_modules = [
+    Extension(
+        'my_module',
+        sources=['my_module/module.cpp', 'my_module/InputData.cpp'],
+        include_dirs=['extern/pybind11/include', 'json/include'],
+        language='c++'
+    ),
+]
+
 setup(
     name='my_module',
     version='1.0',
-    description='My custom module',
-    ext_modules=[module],
+    ext_modules=ext_modules,
+    cmdclass={'build_ext': BuildExt},
 )
