151f2d344bf2838e0adc22b743f9e94a758aff45
bigdatacon <link17892020@gmail.com>
2024-05-24 14:54:46 +0300

    новая ветка - универсальный wheel

--- setup.py
+++ setup.py
@@ -2,11 +2,29 @@ from setuptools import setup, Extension
 import pybind11
 
 setup(
-    name='my_module',
-    version='1.0',
-    ext_modules=[Extension('my_module', ['my_module/module.cpp', 'my_module/InputData.cpp'], include_dirs=[pybind11.get_include()])],
-    setup_requires=['pybind11>=2.5.0'],
-    install_requires=[
-        'pybind11>=2.5.0'
+    name="my_module",
+    version="1.0",
+    author="Your Name",
+    author_email="your.email@example.com",
+    description="A sample C++ extension module",
+    long_description="",
+    ext_modules=[
+        Extension(
+            "my_module",
+            sources=[
+                "my_module/module.cpp",
+                "my_module/InputData.cpp",
+            ],
+            include_dirs=[
+                pybind11.get_include(),
+                "extern/pybind11/include",
+                "json/include"
+            ],
+            language="c++"
+        ),
     ],
+    install_requires=[
+        "pybind11>=2.5.0"
+    ],
+    zip_safe=False,
 )
